1e8b51c8b7219014937b2e8cce7656a413b48647
danielosorio <vdosavh@gmail.com>
2016-06-03 16:35:10 +0100

    TudoAteAoGetArea

--- pathmath_interpolationxy.py
+++ pathmath_interpolationxy.py
@@ -8,6 +8,8 @@ from numpy.testing import assert_almost_equal, assert_approx_equal
 
 import seaborn
 import matplotlib.pyplot as plt
+import bokeh as bk
+from bokeh.io import hplot
 from bokeh.plotting import figure, output_file, show
 
 
@@ -348,7 +350,7 @@ def get_area(x, y, scanning_window=1):
 
     area = area_calc(contour_array)
 
-    return area
+    return area, contour_array
 
 ###
 
@@ -522,9 +524,28 @@ def overlap(xx, yy, title, xlabel, ylabel, legend, fontsize=20, plot_line=['-', 
 #                          #
 ############################
 
-def print_simple_bokeh(x, y)
+
+def print_simple_bokeh(x, y, title, xlabel, ylabel):
+
+    bk.plotting.output_file("scatter_data.html")
+    p = bk.plotting.figure(title=title, x_axis_label=xlabel, y_axis_label=ylabel)
+    p.line(x, y)
+    bk.plotting.show(p)
 
 
+def bokeh_subplot(x, y, title, xlabel, ylabel):
+    bk.plotting.output_file("subplot_data.html")
+    s1 = bk.plotting.figure(title=title[0], x_axis_label=xlabel[0], y_axis_label=ylabel[0])
+    s1.circle(x[0], y[0], size=5, color='firebrick', alpha=0.5)
+    s1.line(x[1], y[1], alpha=0.5, line_width=2, line_dash="dashed")
+
+    s2 = bk.plotting.figure(title=title[1], x_axis_label=xlabel[1], y_axis_label=ylabel[1])
+    s2.circle(x[1], y[1], size=5, color='olive', alpha=0.5)
+    s2.patch(x[1], y[1], alpha=0.5, line_width=2)
+
+    p = hplot(s1, s2)
+
+    bk.plotting.show(p)
 ############################
 #                          #
 #      To Be Deleted       #
@@ -533,13 +554,17 @@ def print_simple_bokeh(x, y)
 
 # Random scatter data
 
-x, y = generate_random_data([-20, 20], [-20, 20], 1000)
+x, y = generate_random_data([-20, 20], [-20, 20], 100)
 
 figure1 = regular_plot(x, y, "Generated Data", "x", "y", plot_line='o')
 
-area = get_area(x, y, scanning_window=0)
+area, contour_array = get_area(x, y, scanning_window=0)
 
 print area
+
+print_simple_bokeh(x, y, "Generated Data", "x", "y")
+
+bokeh_subplot([x, contour_array[:, 0]], [y, contour_array[:, 1]], ["Generated Data", "Contour Plot"], ["x", "x"], ["y", "y"])
 
 
 plt.show()
